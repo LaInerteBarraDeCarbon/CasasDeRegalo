4e95c47600ea4cc6892c0e27371f161886ffa9d6
Damian Alladio <damian.e.alladio@gmail.com>
2017-06-13 20:39:44 -0300

    Estructura Básica de Carpetas

---
+++ test/cadena/CadenaTest.java
@@ -0,0 +1,17 @@
+package cadena;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CadenaTest {
+
+	private static String archivoIn = "Preparacion de Prueba/Lote de Prueba/Entrada/";
+	private static String archivoOut = "Ejecucion de Prueba/Salida Obtenida/";
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
